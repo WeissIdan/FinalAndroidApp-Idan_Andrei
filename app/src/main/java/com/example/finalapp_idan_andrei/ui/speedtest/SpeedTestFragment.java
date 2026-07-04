85d970b72e02f6a81b2e55f97efc3a64c3e6065c
idan_weiss <i.weiss.2006@gmail.com>
2026-07-04 19:08:59 +0300

    added navbar, destinations in navigation, fragment container view in activity_main

---
+++ app/src/main/java/com/example/finalapp_idan_andrei/ui/speedtest/SpeedTestFragment.java
@@ -0,0 +1,31 @@
+package com.example.finalapp_idan_andrei.ui.speedtest;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.finalapp_idan_andrei.databinding.FragmentSpeedTestBinding;
+
+public class SpeedTestFragment extends Fragment {
+
+    private FragmentSpeedTestBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                              @Nullable Bundle savedInstanceState) {
+        binding = FragmentSpeedTestBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
