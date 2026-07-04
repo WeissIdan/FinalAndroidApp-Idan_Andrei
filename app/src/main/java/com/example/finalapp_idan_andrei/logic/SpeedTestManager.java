79bfbf470630d4a759b84fbd69d7f5699ca72286
andrei113322 <andrei.iutish@gmail.com>
2026-07-04 20:19:18 +0300

    Finished the speed test logic, created an java file with fundtion and an tester, ready to make the frontend elements

---
+++ app/src/main/java/com/example/finalapp_idan_andrei/logic/SpeedTestManager.java
@@ -0,0 +1,8 @@
+package com.example.finalapp_idan_andrei.logic;
+
+/**
+ * Manager class to handle speed test logic (Ping, Download, Upload).
+ */
+public class SpeedTestManager {
+    // Logic will be implemented here
+}
